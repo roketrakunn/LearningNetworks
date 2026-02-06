982f56a77d733c30f825fdb7dda7fe9b1c06c380
OKuhle <okuhlexesibe0@gmail.com>
2026-02-06 18:04:13 +0000

    Documentation I guess

--- echo_server/client.go
+++ echo_server/client.go
@@ -1,3 +1,11 @@
+/**
+ + This is a basic client that sends a message to a local server
+ + It then reads the response from the client and prints to the screen 
+ + What it teaches me ? How simple web stuff is... definetley overrated
+ + The server sends a message back 
+*/
+
+
 package main
 
 import (
@@ -9,6 +17,11 @@ import (
 
 
 func main() { 
+	/**
+	+ tcp connection  localhost on port 80(http)
+	+ if err is not null , the connection has failed 
+	+ Confirm sever is up.
+	*/
 	conn , err := net.Dial("tcp", "Localhost:8080")
 
 	if err != nil { 
@@ -17,31 +30,36 @@ func main() {
 		os.Exit(1)
 	}
 
-	defer conn.Close() // close the connection when done 
+	//at end of main close the connection(just good practice)
+	defer conn.Close() 
 
 	fmt.Println("Connected to echo server")
 	
 
 	//send the message
-	message := "Hello echo sever.\n"
+	message := "Hello echo sever.\n" //message you uwanna send
 	fmt.Printf("Sending message :%s" , message)
 
-	_ , err = conn.Write([]byte(message)) 
-
+	_ , err = conn.Write([]byte(message)) // write ro the file ( see I told you everhting is a file)
 	if err != nil { 
 		fmt.Fprintf(os.Stderr , "Error writing: %v\n", err)
 		os.Exit(1)
 	}
 
-	reader := bufio.NewReader(conn)
+	/**
+	+ Create a buffer reader
+	+ Will read the response from server
+	+ In this case the resposne will be "ECHO: <message>"
+	*/
+	reader := bufio.NewReader(conn) 
 
-	response , err := reader.ReadString('\n')
+	response , err := reader.ReadString('\n')  // save response to response
 	
 	if err != nil { 
 		fmt.Fprintf(os.Stderr , " Error reading: %v\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Printf("Recieved: %s" , response)
+	fmt.Printf("Recieved: %s" , response) //print the response to your terminal ...
 	fmt.Println("DONE!")
 }
